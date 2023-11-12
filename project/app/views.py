537becc025a21dae662d1861d6c233883493b6a0
shazid-hasan <amj.shazidhasan@gmail.com>
2023-11-12 10:27:37 +0600

    first commit

---
+++ project/app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from app.models import Employees
+
+def Index(request):
+    emp=Employees.objects.all()
+
+    context ={
+        'emp':emp,
+    }
+
+
+    return render(request,'index.html',context)
+
+# Create your views here.
+def Add(request):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        address=request.POST.get('address')
+        phone=request.POST.get('phone')
+
+        emp=Employees(
+            name =name,
+            email=email,
+            address=address,
+            phone=phone,
+        )
+        emp.save()
+        return redirect('home')
+
+    return render(request,'index.html')
+
+def Edit(request):
+    emp=Employees.objects.all()
+
+    context ={
+        'emp':emp,
+    }
+    return redirect(request,'index.html',context)
